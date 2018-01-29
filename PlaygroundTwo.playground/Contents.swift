00b59b891930d6c818cec86c4e59f74bf6ddfcd8
Carlton Hui <carltonhui@gmail.com>
2018-01-29 10:40:13 -1000

    Completed Hackwich Two

--- PlaygroundTwo.playground/Contents.swift
+++ PlaygroundTwo.playground/Contents.swift
@@ -3,3 +3,22 @@
 import UIKit
 
 var str = "Hello, playground"
+
+var goodGrade = "I've been doing all my work"
+if goodGrade == "I've been doing all my work"
+{
+    print("I'm doing well in this class")
+}
+
+if goodGrade == "I'm not doing very well"
+{
+    print("I'm doing well in this class")
+}
+if goodGrade == "I've been doing all my work"
+{
+    print ("I'm doing well in this class")
+}
+else
+{
+    print ("I'm not doing too well")
+}
